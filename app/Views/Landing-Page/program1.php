686a0eaec51378f9eee0e6d5807e52d0edd813fe
Lathifah <lathifah.putri21@students.unila.ac.id>
2024-01-17 15:55:13 +0700

    update Landing Page

--- app/Views/Landing-Page/program1.php
+++ app/Views/Landing-Page/program1.php
@@ -1,109 +1,116 @@
 <?= $this->extend('layouts/landing-page/_header') ?>
 <?= $this->section('content') ?>
-  <main id="main">
 
-    <!-- ======= Breadcrumbs ======= -->
-    <section class="breadcrumbs">
-      <div class="container">
+<main id="main">
 
-        <ol>
-          <li><a href="<?= base_url('/')?>">Beranda</a></li>
-          <li>Pondok Pesantren dan Rumah Ibadah</li>
-        </ol>
-        <h2>Program Bantuan Dana Hibah Pondok Pesantren dan Rumah Ibadah</h2>
-
+  <!-- ======= Breadcrumbs ======= -->
+  <section class="breadcrumbs">
+    <div class="container">
+  
+      <ol>
+        <li><a href="<?= base_url('/')?>">Beranda</a></li>
+        <li>Pondok Pesantren dan Rumah Ibadah</li>
+      </ol>
+      <h2>Program Bantuan Dana Hibah Pondok Pesantren dan Rumah Ibadah</h2>
+  
+    </div>
+  </section>
+  <!-- End Breadcrumbs -->
+  
+  <!-- ======= Blog Single Section ======= -->
+  <section id="blog" class="blog">
+    <div class="container" data-aos="fade-up">
+  
+      <div class="row">
+  
+        <div class="col-lg-12 entries">
+  
+          <article class="entry entry-single">
+  
+            <div class="entry-img">
+              <img src="<?= base_url("assets/img/") ?>" alt="" class="img-fluid">
+            </div>
+  
+            <h2 class="entry-title">
+              <a href="<?= base_url('program1')?>">Persyaratan Pendaftar</a>
+            </h2>
+            
+            <ol class="list-group list-group-numbered">
+              <li class="list-group-item">Permohonan diketahui Minimal Pamong/Kepala Desa setempat</li>
+              <li class="list-group-item">Susunan Pengurus/Panitia diketahui KUA/Pamong/Kepala Desa setempat</li>
+              <li class="list-group-item">Rencana Anggaran Belanja (RAB)</li>
+              <li class="list-group-item">Foto Bangunan/Foto Kegiatan</li>
+              <li class="list-group-item">Akte Notaris, Kemenkumham, Izin Operasional</li>
+              <li class="list-group-item">Foto copy KTP Ketua dan Sekretaris (Masih aktif)</li>
+              <li class="list-group-item">No. Rek Bank Lampung atas nama PonPes/Lembaga</li>
+              <li class="list-group-item">No. NPWP atas Nama Ketua/Rumah Ibadah/Pengurus</li>
+              <li class="list-group-item">Stempel Asli dan Basah</li>
+              <li class="list-group-item">Surat Keterangan Domisili</li>
+              <li class="list-group-item">Lain-lain</li>
+            </ol>
+            
+            <div class="mt-4 box pricing text-center">
+              <a href="/pendaftaran" class="btn-buy">Ajukan Bantuan <i class="bi bi-arrow-right"></i></a>
+            </div>
+  
+          <!-- <div class="entry-content">
+            <p>
+                
+            </p>
+  
+            <p>
+                2. 
+            </p>
+  
+            <p>
+                3. 
+            </p>
+  
+            <p>
+                4. 
+            </p>
+  
+            <p>
+                5. 
+            </p>
+  
+            <p>
+                6. 
+            </p>
+  
+            <p>
+                7. 
+            </p>
+  
+            <p>
+                8. 
+            </p>
+  
+            <p>
+                9. 
+            </p>
+  
+            <p>
+                10. 
+            </p>
+  
+            <p>
+                11. Lain-lain
+            </p>
+  
+          </div> -->
+  
+          </article><!-- End blog entry -->
+  
+        </div><!-- End blog entries list -->
+  
       </div>
-    </section><!-- End Breadcrumbs -->
+  
+    </div>
+  </section>
+  <!-- End Blog Single Section -->
 
-    <!-- ======= Blog Single Section ======= -->
-    <section id="blog" class="blog">
-      <div class="container" data-aos="fade-up">
+</main>
+<!-- End #main -->
 
-        <div class="row">
-
-          <div class="col-lg-12 entries">
-
-            <article class="entry entry-single">
-
-              <div class="entry-img">
-                <img src="<?= base_url("assets/img/") ?>" alt="" class="img-fluid">
-              </div>
-
-              <h2 class="entry-title">
-                <a href="<?= base_url('program1')?>">Persyaratan Pendaftar</a>
-              </h2>
-
-              <!-- <div class="entry-meta">
-                <ul>
-                  <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.html">John Doe</a></li>
-                  <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-                  <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="blog-single.html">12 Comments</a></li>
-                </ul>
-              </div> -->
-
-              <div class="entry-content">
-                <p>
-                  Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-                  Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
-                </p>
-
-                <p>
-                  Sit repellat hic cupiditate hic ut nemo. Quis nihil sunt non reiciendis. Sequi in accusamus harum vel aspernatur. Excepturi numquam nihil cumque odio. Et voluptate cupiditate.
-                </p>
-
-                <blockquote>
-                  <p>
-                    Et vero doloremque tempore voluptatem ratione vel aut. Deleniti sunt animi aut. Aut eos aliquam doloribus minus autem quos.
-                  </p>
-                </blockquote>
-
-                <p>
-                  Sed quo laboriosam qui architecto. Occaecati repellendus omnis dicta inventore tempore provident voluptas mollitia aliquid. Id repellendus quia. Asperiores nihil magni dicta est suscipit perspiciatis. Voluptate ex rerum assumenda dolores nihil quaerat.
-                  Dolor porro tempora et quibusdam voluptas. Beatae aut at ad qui tempore corrupti velit quisquam rerum. Omnis dolorum exercitationem harum qui qui blanditiis neque.
-                  Iusto autem itaque. Repudiandae hic quae aspernatur ea neque qui. Architecto voluptatem magni. Vel magnam quod et tempora deleniti error rerum nihil tempora.
-                </p>
-
-                <h3>Et quae iure vel ut odit alias.</h3>
-                <p>
-                  Officiis animi maxime nulla quo et harum eum quis a. Sit hic in qui quos fugit ut rerum atque. Optio provident dolores atque voluptatem rem excepturi molestiae qui. Voluptatem laborum omnis ullam quibusdam perspiciatis nulla nostrum. Voluptatum est libero eum nesciunt aliquid qui.
-                  Quia et suscipit non sequi. Maxime sed odit. Beatae nesciunt nesciunt accusamus quia aut ratione aspernatur dolor. Sint harum eveniet dicta exercitationem minima. Exercitationem omnis asperiores natus aperiam dolor consequatur id ex sed. Quibusdam rerum dolores sint consequatur quidem ea.
-                  Beatae minima sunt libero soluta sapiente in rem assumenda. Et qui odit voluptatem. Cum quibusdam voluptatem voluptatem accusamus mollitia aut atque aut.
-                </p>
-                <img src="assets/img/blog/blog-inside-post.jpg" class="img-fluid" alt="">
-
-                <h3>Ut repellat blanditiis est dolore sunt dolorum quae.</h3>
-                <p>
-                  Rerum ea est assumenda pariatur quasi et quam. Facilis nam porro amet nostrum. In assumenda quia quae a id praesentium. Quos deleniti libero sed occaecati aut porro autem. Consectetur sed excepturi sint non placeat quia repellat incidunt labore. Autem facilis hic dolorum dolores vel.
-                  Consectetur quasi id et optio praesentium aut asperiores eaque aut. Explicabo omnis quibusdam esse. Ex libero illum iusto totam et ut aut blanditiis. Veritatis numquam ut illum ut a quam vitae.
-                </p>
-                <p>
-                  Alias quia non aliquid. Eos et ea velit. Voluptatem maxime enim omnis ipsa voluptas incidunt. Nulla sit eaque mollitia nisi asperiores est veniam.
-                </p>
-
-              </div>
-
-              <div class="entry-footer">
-                <i class="bi bi-folder"></i>
-                <ul class="cats">
-                  <li><a href="#">Business</a></li>
-                </ul>
-
-                <i class="bi bi-tags"></i>
-                <ul class="tags">
-                  <li><a href="#">Creative</a></li>
-                  <li><a href="#">Tips</a></li>
-                  <li><a href="#">Marketing</a></li>
-                </ul>
-              </div>
-
-            </article><!-- End blog entry -->
-
-          </div><!-- End blog entries list -->
-
-        </div>
-
-      </div>
-    </section><!-- End Blog Single Section -->
-
-  </main><!-- End #main -->
 <?= $this->endSection() ?>
